5f1b8421b4ee99dbf64c6723eb0b15dc6353f95f
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-01 11:02:55 -0500

    Have the add race method complete

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe Election do
 
     expect(election.year).to eq('2022')
   end
+
+  it 'has races' do
+    election = Election.new('2022')
+
+    expect(election.races).to eq([])
+  end
+
+  it 'can add races' do
+    election = Election.new('2022')
+    race1 = Race.new("Virginia District 4 Representative")
+    race2 = Race.new("Texas Governor")
+
+    election.add_race(race1)
+    election.add_race(race2)
+
+    expect(election.races).to eq([race1, race2])
+  end
+
 end
